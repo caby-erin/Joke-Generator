eb7eeccf289bb294f924d230b6a4976e70cc4940
caby-erin <caby27@gmail.com>
2023-05-20 20:30:27 -0500

    when button is clicked a joke is shown on the dom

--- public/main.js
+++ public/main.js
@@ -2,24 +2,41 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import getRequest from '../api/promises';
+import renderToDom from '../utils/renderToDom';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
+    <h1> Joke Generator 3000</h1>
+    <small>It's chuckle time</small><br />
+    <button class="btn btn-danger" id="get-joke">Generate a Joke</button><br />
     <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
   `;
   console.warn('YOU ARE UP AND RUNNING!');
 
-  document
+  /* document
     .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+    .addEventListener('click', () => console.warn('You clicked that button!')); */
 
   // USE WITH FIREBASE AUTH
   // ViewDirectorBasedOnUserAuthStatus();
 };
 
-init();
+const newJoke = () => {
+  getRequest().then((item) => {
+    renderToDom('#joke-container', item.setup);
+  });
+};
+
+const events = () => {
+  document.querySelector('#get-joke').addEventListener('click', () => {
+    getRequest().then(newJoke);
+  });
+};
+
+const startApp = () => {
+  init();
+  events();
+};
+
+startApp();
